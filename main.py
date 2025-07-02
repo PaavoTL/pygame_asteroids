c148d2d10f4c20a3e470b01914cfb256711b964e
PaavoTL <paavo.linkala@hotmail.com>
2025-07-02 13:40:21 +0300

    added Clock and rate limit

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         screen.fill(000000)
@@ -15,6 +17,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
